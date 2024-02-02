a34b8bbf690a3122a9162a3cae907cb2359107d9
Mannyluccio <mtosti@engagepeople.com>
2024-02-02 17:56:52 +0100

    add translation extractor

--- src/app/home/home.guard.ts
+++ src/app/home/home.guard.ts
@@ -1,9 +1,9 @@
-import {CanActivate, Router} from '@angular/router';
+import {Router} from '@angular/router';
 import { Injectable } from '@angular/core';
 import {AuthService} from "../auth/auth.service";
 
 @Injectable()
-export class HomeGuard implements CanActivate {
+export class HomeGuard{
   constructor(private authService: AuthService,
               private router: Router) {}
 
